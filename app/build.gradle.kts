9bd56f1e5e8d07b9ed984fcc9d36d625dc47809a
ClearName666 <pavelstorozev522@gmail.com>
2024-05-30 11:02:55 +0800

    0.0.1 0024 1) сделал диагностику у m32 2) начал делать пользовательские шаблоны

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    /*implementation("androidx.room:room-runtime:2.6.1")
-    kapt()*/
 }
